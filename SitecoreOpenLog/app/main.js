ee4071008e084da6b06cc8b039961e28a8840009
Antonytm <antontishchenko@gmail.com>
2014-11-12 00:18:10 +0200

    Added settings page

--- SitecoreOpenLog/app/main.js
+++ SitecoreOpenLog/app/main.js
@@ -4,17 +4,35 @@
     plugin.setAttribute("style", "width:0px; height: 0px;");
     document.documentElement.appendChild(plugin);
     console.log(plugin.getPlatform());
-    console.log(plugin.listFiles("C:\\inetpub\\wwwroot\\"));
-    
+    chrome.storage.sync.get({
+        wwwrootFolder: 'c:\\inetpub\\wwwroot\\'
+    }, function (items) {
+        var wwwrootFolder = items.wwwrootFolder;
+        console.log(wwwrootFolder);
+        showPopup(wwwrootFolder);
+    });
+}
+
+function showPopup(wwwrootFolder) {
     chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs) {
+        var start = (new Date()).getTime();
         var parser = document.createElement('a');
         parser.href = tabs[0].url;
         console.log(parser.host);
-        var a = plugin.lastFileByTemplate("C:\\inetpub\\wwwroot\\" + parser.host + "\\Data\\logs\\", "log*.txt");
+        var a = plugin.lastFileByTemplate(wwwrootFolder + parser.host + "\\Data\\logs\\", "log*.txt");
+        var getFile = (new Date()).getTime();
+        console.log(getFile - start);
         console.log(a);
-        logFilePath = "C:\\inetpub\\wwwroot\\" + parser.host + "\\Data\\logs\\" + a[0].name;
+        logFilePath = wwwrootFolder + parser.host + "\\Data\\logs\\" + a[0].name;
         console.log(logFilePath);
         fileContent = plugin.getTextFile(logFilePath);
-        window.document.getElementById("log").innerHTML = fileContent.replace("\n\r","<br/>");
+        var fileRead = (new Date()).getTime();
+        console.log(fileRead - getFile);
+        window.document.getElementById("log").innerHTML = fileContent.replace(/(?:\r\n|\r|\n)/g, '<br />') + "<div id='bottom'></div>";
+        console.log(fileContent);
+        var popupShown = (new Date()).getTime();
+        console.log(popupShown - fileRead);
+        document.getElementById('bottom').scrollIntoView();
+        window.setTimeout(function () { top(); }, 2000);
     });
 }
---
+++ SitecoreOpenLog/app/options.js
@@ -0,0 +1,25 @@
+﻿// Saves options to chrome.storage
+function save_options() {
+    var wwwrootFolder = document.getElementById('wwwrootFolder').value;
+    chrome.storage.sync.set({
+        wwwrootFolder: wwwrootFolder
+    }, function () {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function () {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+function restore_options() {
+    chrome.storage.sync.get({
+        wwwrootFolder: 'c:\\inetpub\\wwwroot\\'
+    }, function (items) {
+        document.getElementById('wwwrootFolder').value = items.wwwrootFolder;
+        console.log(items.wwwrootFolder);
+    });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',save_options);
